a4fd50e6a85fd36810001d96b5f9c53880aa6c3b
Javier M Colombini <javier-colombini@outlook.com>
2024-04-30 09:34:28 -0300

    app de prueba

---
+++ src/main/java/com/aplication/primeraapp/PrimeraappApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.primeraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplication/primeraapp/PrimeraappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.primeraapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
